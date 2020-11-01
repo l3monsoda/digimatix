b63a129d70bf34d0fe457d7fe5894a3fb7612079
Ilya Sovin <levenus8supremus@icloud.com>
2020-11-02 02:23:59 +0300

    main slider styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,20 @@
 window.onload = function () {
 
-    
-$('.offers-slider').slick({
+
+  $('.offers-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
+    arrows: true,
   });
- 
+
+  $('.slider-prev').on('click', function() {
+    $('.offers-slider').slick('slickPrev');
+  });
+
+  $('.slider-next').on('click', function() {
+    $('.offers-slider').slick('slickNext');
+  });
+
 }
