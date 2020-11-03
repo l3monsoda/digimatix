6c10c683fd45309dd8a492f2715dcf6decd806b9
Ilya Sovin <levenus8supremus@icloud.com>
2020-11-03 07:41:30 +0300

    form effects added

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,63 @@
 window.onload = function () {
 
-
   $('.offers-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
     arrows: true,
+    responsive: [{
+        breakpoint: 1210,
+        settings: {
+            rows: 2,
+            slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesPerRow: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesPerRow: 1
+        }
+      }
+    ]
   });
 
-  $('.slider-prev').on('click', function() {
+  $('.slider-prev').on('click', function () {
     $('.offers-slider').slick('slickPrev');
   });
 
-  $('.slider-next').on('click', function() {
+  $('.slider-next').on('click', function () {
     $('.offers-slider').slick('slickNext');
   });
+
+  var input = $('.form-input')
+
+  input.on('focus', function () {
+    $(this).prev().prev().fadeOut('fast')
+    $(this).prev().fadeIn('fast')
+  })
+
+  input.on('blur', function () {
+    if ($(this).val().length >= 1) {
+      $(this).prev().prev().fadeOut('fast')
+      $(this).prev().fadeIn('fast')
+    }
+
+    if (!$(this).val().length) {
+      $(this).prev().fadeOut('fast')
+      $(this).prev().prev().fadeIn('fast')
+    }
+  })
+
+  $('.offers').on('click', function() {
+    $('.offers').removeClass('offers-active')
+    $(this).addClass('offers-active')
+  })
 
 }
